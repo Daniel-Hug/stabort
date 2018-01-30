fa7230ca5cf49cd7e2ea483105ef1ef29bcae805
Daniel Hug <daniel@hugwebdesign.com>
2018-01-29 16:51:44 -0800

    Update stabort.js

--- stabort.js
+++ stabort.js
@@ -13,20 +13,13 @@
 	return function sortBy(objs, prop, order) {
 		// run sort on clone so as not to modify original
 		var arrClone = [].slice.call(objs, 0);
+		var desc = order.indexOf('desc') >= 0;
 
-		function compareDesc(a, b) {
-			if (b[prop] < a[prop]) return -1;
-			if (b[prop] > a[prop]) return 1;
+		arrClone.sort(function(a, b) {
+			if (a[prop] < b[prop]) return desc ?  1 : -1;
+			if (a[prop] > b[prop]) return desc ? -1 :  1;
 			return objs.indexOf(a) - objs.indexOf(b);
-		}
-
-		function compareAsc(a, b) {
-			if (a[prop] < b[prop]) return -1;
-			if (a[prop] > b[prop]) return 1;
-			return objs.indexOf(a) - objs.indexOf(b);
-		}
-
-		arrClone.sort(order.indexOf('desc') >= 0 ? compareDesc : compareAsc);
+		});
 
 		return arrClone;
 	};
